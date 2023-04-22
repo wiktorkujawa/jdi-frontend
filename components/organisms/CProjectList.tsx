c26d41781a3af294e8e85fdbcb8e0780a1584e08
wiktorkujawa <wiktorkujawa1993@gmail.com>
2023-04-23 01:41:07 +0200

    push changes after adding an env variables

--- components/organisms/CProjectList.tsx
+++ components/organisms/CProjectList.tsx
@@ -1,14 +1,14 @@
-import { Project } from '@/interfaces';
-import React from 'react'
-import MProjectItem from '@/components/molecules/MProjectItem';
-import styles from 'theme/components/organisms/CProjectList.module.css'
-import classNames from 'classnames';
-import { Button } from '@/interfaces';
-import { Upload } from '@/interfaces';
+import { Project } from "@/interfaces";
+import React from "react";
+import MProjectItem from "@/components/molecules/MProjectItem";
+import styles from "theme/components/organisms/CProjectList.module.css";
+import classNames from "classnames";
+import { Button } from "@/interfaces";
+import { Upload } from "@/interfaces";
 
 type Props = {
   field: {
-    projectsList: Project[],
+    projectsList: Project[];
     id: string;
     name?: string;
     mediaUrl?: string;
@@ -17,25 +17,26 @@ type Props = {
     updatedAt: string;
     button: Button;
     media: Upload;
-  }
-}
+  };
+};
 
 const CProjectList = ({ field: { projectsList, ...main } }: Props) => {
-
   return (
-    <section className={classNames(styles['c-project-list'], 'o-container o-container--lg my-16')}>
+    <section
+      className={classNames(
+        styles["c-project-list"],
+        "o-container o-container--lg my-16"
+      )}
+    >
+      {main && <MProjectItem key={main.id} field={main} main />}
 
-      {main && <MProjectItem key={main.id} field={main} main /> }
-
-      <div id="list" className={styles['c-project-list--list-counter']}>
-      {
-        projectsList?.map((field) => (
+      <div id="list" className={styles["c-project-list--list-counter"]}>
+        {projectsList?.map((field) => (
           <MProjectItem key={field.id} field={field} />
-        ))
-      }
+        ))}
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default CProjectList
+export default CProjectList;
