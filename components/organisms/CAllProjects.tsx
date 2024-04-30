35d939e0e97d48b2169e00532e9dd6f1256c9e13
wiktorkujawa <wiktorkujawa1993@gmail.com>
2024-04-30 23:40:37 +0200

    feature/remove-loadable - removed loadable and formatted multiple components

--- components/organisms/CAllProjects.tsx
+++ components/organisms/CAllProjects.tsx
@@ -3,27 +3,27 @@ import { Button, Upload } from "@/interfaces";
 import CProjectList from "@/components/organisms/CProjectList";
 
 type ProjectListProps = {
-    projectsList: Project[];
-    id: string;
-    name?: string;
-    mediaUrl?: string;
-    description?: string;
-    createdAt: string;
-    updatedAt: string;
-    buttons: GroupElement<Button, "button">[];
-    button: Button;
-    media: Upload;
+  projectsList: Project[];
+  id: string;
+  name?: string;
+  mediaUrl?: string;
+  description?: string;
+  createdAt: string;
+  updatedAt: string;
+  buttons: GroupElement<Button, "button">[];
+  button: Button;
+  media: Upload;
 };
 
-  const getProjectsData = async () => {
-    const res = await fetch(`${process.env.API_URL}globals/projectList`, {
-      next: {
-        tags: ["projectsList"],
-      },
-    });
-    const data: ProjectListProps = await res.json();
-    return data;
-  };
+const getProjectsData = async () => {
+  const res = await fetch(`${process.env.API_URL}globals/projectList`, {
+    next: {
+      tags: ["projectsList"],
+    },
+  });
+  const data: ProjectListProps = await res.json();
+  return data;
+};
 
 const CAllProjects = async () => {
   const { projectsList, ...main } = await getProjectsData();
@@ -31,7 +31,7 @@ const CAllProjects = async () => {
     <CProjectList field={{
       projectsList: projectsList,
       ...main
-    }}  />
+    }} />
   );
 };
 
