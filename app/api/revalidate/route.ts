a5fac881c8d595ff8f630154c0f0e77a02d3bbea
wiktorkujawa <wiktorkujawa1993@gmail.com>
2024-05-05 11:35:03 +0200

    feature/slider - restored revalidation request

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -13,12 +13,19 @@ export async function POST(req: NextRequest, res: NextResponse) {
     // This should be the actual path not a rewritten path
     // e.g. for "/blog/[slug]" this should be "/blog/post-1"
     const body: { slug: string } = await req.json();
-    const slug = !body?.slug ? '' : body?.slug[0] ==="/" ? body?.slug : '/'+body?.slug;
-    await revalidatePath(slug || '');
+    if(body.slug) {
+      const slug = body?.slug[0] ==="/" ? body?.slug : '/'+body?.slug;
+      await revalidatePath(slug);
 
-    return new Response(`Revalidated ${slug} page`, {
-      status: 200
-    })
+      return new Response(`Revalidated ${slug} page`, {
+        status: 200
+      })
+    }
+    else {
+      return new Response('Missing slug', {
+        status: 404
+      })
+    }
   } catch (err) {
     // If there was an error, Next.js will continue
     // to show the last successfully generated page
