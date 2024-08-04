a61ac43161802afb294f403d3621e683732cf504
wiktorkujawa <wiktorkujawa1993@gmail.com>
2024-08-04 23:41:48 +0200

    feature/menu-shadow - add shadow for menu

--- components/molecules/MNavigation/index.tsx
+++ components/molecules/MNavigation/index.tsx
@@ -49,7 +49,7 @@ const MNavigation: FC<Props> = ({ nav }) => {
         " dark:text-dark-font-primary text-theme-font-primary"
       )}
     >
-      <div className={clsx("absolute transition-transform left-0 top-0 h-full w-full -translate-y-full bg-theme-bg-window dark:bg-dark-bg-window", (scrolling || (openNav && !isDesktop)) && "translate-y-0")} />
+      <div className={clsx("absolute transition-transform left-0 top-0 h-full w-full -translate-y-full bg-theme-bg-window dark:bg-dark-bg-window", (scrolling || (openNav && !isDesktop)) && "translate-y-0 shadow-menu")} />
       <div className={styles.content}>
         <Link aria-label="Homepage" className='z-10' href={"/"}>
           <Logo className={clsx(styles.logo, "dark:fill-white")} />
