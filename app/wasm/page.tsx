644025976ba73b5495b4a02482be0edfeefbb7f8
wiktorkujawa <wiktorkujawa1993@gmail.com>
2024-08-05 00:48:13 +0200

    feature/fix-wasm-page-height - fixed wasm page height issue

--- app/wasm/page.tsx
+++ app/wasm/page.tsx
@@ -20,45 +20,46 @@ export const generateMetadata = async () => {
 
 const Wasm = async () => {
   const { customComponents, subpages } = await getPageData();
+  console.log(subpages);
   return (
     <>
-        <div className="relative h-screen o-container o-container--lg my-16">
-          <CHeadingCopyBlock
-            field={{
-              level: "h1",
-              heading: "Web Assembly",
-              copy: [
-                {
-                  text: "Discover the power of Web Assembly (WASM) apps - revolutionizing web development with enhanced interactivity and performance.",
-                  type: "p",
-                  indent: 0,
-                },
-              ],
-            }}
-          />
-          <CProjectList
-            // @ts-ignore
-            field={{
-              projectsList:
-                subpages?.map((subpage) => {
-                  return {
-                    name: subpage.name,
-                    media: subpage.meta.image,
-                    mediaUrl: subpage.slug,
-                    button: {
-                      text: "Check it out",
-                      url: subpage.slug,
-                    },
-                    createdAt: subpage.createdAt,
-                    id: subpage.id,
-                    updatedAt: subpage.updatedAt,
-                  };
-                }) || [],
-            }}
-          />
-        </div>
-        <div />
-        <LCustomComponents field={customComponents} />
+      <div className="relative o-container o-container--lg my-16">
+        <CHeadingCopyBlock
+          field={{
+            level: "h1",
+            heading: "Web Assembly",
+            copy: [
+              {
+                text: "Discover the power of Web Assembly (WASM) apps - revolutionizing web development with enhanced interactivity and performance.",
+                type: "p",
+                indent: 0,
+              },
+            ],
+          }}
+        />
+        <CProjectList
+          // @ts-ignore
+          field={{
+            projectsList:
+              subpages?.map((subpage) => {
+                return {
+                  name: subpage.name,
+                  media: subpage.meta.image,
+                  mediaUrl: subpage.slug,
+                  button: {
+                    text: "Check it out",
+                    url: subpage.slug,
+                  },
+                  createdAt: subpage.createdAt,
+                  id: subpage.id,
+                  updatedAt: subpage.updatedAt,
+                };
+              }) || [],
+          }}
+        />
+      </div>
+      <div />
+      <LCustomComponents field={customComponents} />
     </>
   );
 };
